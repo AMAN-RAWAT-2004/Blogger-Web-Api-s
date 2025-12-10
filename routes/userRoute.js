294460178062e6c82aa884e78263fdad4fe04d7a
AmandeepRawat <Ar464163@gmail.com>
2025-12-11 01:04:54 +0530

    new changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,9 @@ const userController = require('./../controllers/userController')
 Router.get('/pending', authController.protect, authController.RestrictTo('admin'), adminController.getAllPendingRequests)
 Router.post('/register', authController.roleApprovelCheck, authController.registerUser)
 Router.post('/login', authController.loginUser)
+Router.post('/forgetpassword', authController.forgetPassword)
+Router.patch('/resetpassword/:token', authController.resetPassword)
+
 Router.put('/pending/approve/:id', authController.protect, authController.RestrictTo('admin'), adminController.approveUser)
 Router.put('/pending/reject/:id', authController.protect, authController.RestrictTo('admin'), adminController.rejectedUser)
 
