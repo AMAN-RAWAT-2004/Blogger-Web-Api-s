23dba8374f5cab42a914906b75e6cd8b4715e300
AmandeepRawat <Ar464163@gmail.com>
2025-12-11 01:04:20 +0530

    new changes

--- routes/authorRoute.js
+++ routes/authorRoute.js
@@ -4,5 +4,5 @@ const authorController=require('./../controllers/authorController')
 const authenticateController=require('./../controllers/authenticateController')
 
 Router.route('/').get(authenticateController.protect,authenticateController.RestrictTo('reader'),authorController.getAllAuthor)
-
+Router.route('/:id').get(authenticateController.protect,authenticateController.RestrictTo('reader'),authorController.getAuthour)
 module.exports=Router;
